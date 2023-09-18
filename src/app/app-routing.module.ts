d5eb9cd12606cbc4bc3193dac3ca14542388eeea
Lubomyr Lypetskyi <liubomyr.lypetskyi@gmail.com>
2023-09-18 23:07:35 +0300

    added admin menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
@@ -12,8 +13,10 @@ const routes: Routes = [
       { path: '', component: MainPageComponent },
       { path: 'product/:id', component: ProductPageComponent },
       { path: "cart", component: CartPageComponent }
-
     ]
+  },
+  {
+    path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
   }
 ];
 
